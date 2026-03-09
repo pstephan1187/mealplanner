b739d214a343dce59714a1d93b0267cafd0105c6
Patrick Stephan <pstephan1187@gmail.com>
2026-03-09 17:18:26 -0500

    Refactor: Remove unused sidebar footer navigation

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
 import {
-    BookOpen,
     CalendarDays,
     Carrot,
     ClipboardList,
     CookingPot,
-    Folder,
     LayoutGrid,
 } from 'lucide-vue-next';
 
-import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import {
@@ -58,19 +55,6 @@ const mainNavItems: NavItem[] = [
         icon: ClipboardList,
     },
 ];
-
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
-];
 </script>
 
 <template>
@@ -92,7 +76,6 @@ const footerNavItems: NavItem[] = [
         </SidebarContent>
 
         <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
             <NavUser />
         </SidebarFooter>
     </Sidebar>
